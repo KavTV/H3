d30c3741b8a2921fce89d468f02400ca84bc448f
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2022-05-10 14:35:20 +0200

    trump

---
+++ Angular/Trumps/src/app/text-replacer.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TextReplacerPipe } from './text-replacer.pipe';
+
+describe('TextReplacerPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TextReplacerPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ Angular/Trumps/src/app/text-replacer.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'textReplacer'
+})
+export class TextReplacerPipe implements PipeTransform {
+
+  transform(value: string, args?: any): string {
+    let newString = value;
+    newString = newString.replace("stupid","bad");
+
+    return newString;
+  }
+
+}
