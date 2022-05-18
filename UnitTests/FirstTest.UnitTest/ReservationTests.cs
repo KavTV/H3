d2feb53d8726df08599f08ecd0c9456a12e8a7c0
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2022-05-18 13:00:50 +0200

    Unit tests

---
+++ UnitTests/FirstTest.UnitTest/ReservationTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace FirstTest.UnitTest
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            Reservation res = new Reservation(new User());
+
+            bool result = res.CanBeCancelledBy(new User() { IsAdmin=true});
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_UserMadeReservation_ReturnsTrue()
+        {
+            User user = new User() { IsAdmin = false };
+
+            Reservation res = new Reservation(user);
+
+            bool result = res.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_UserIsNotAdminAndNoReservation_ReturnsFalse()
+        {
+            User user = new User() { IsAdmin = false };
+
+            Reservation res = new Reservation(new User());
+
+            bool result = res.CanBeCancelledBy(user);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ UnitTests/FirstTest/FirstTest/Reservation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTest
+{
+    public class Reservation
+    {
+        public User MadeBy;
+
+        public Reservation(User user)
+        {
+            MadeBy = user;
+        }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            if (user.IsAdmin)
+            {
+                return true;
+            }
+            if (MadeBy == user)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
