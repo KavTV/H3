436defe2a67cc2ff28ec3e0b16a36a8e4aea5a33
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2022-06-21 10:42:06 +0200

    Randomness task

---
+++ Encryption/Randomness/Randomness/Encrypter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomness
+{
+    public class Encrypter
+    {
+
+        static char[] code = 
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ".ToCharArray();
+
+        public static string Encrypt(string message)
+        {
+            char[] messageChar = message.ToCharArray();
+            string newString = "";
+            for (int i = 0; i < messageChar.Length; i++)
+            {
+                //Make it in uppercase when searching
+                char ch = Char.ToUpper(messageChar[i]);
+                //Find index in array where character is present
+                int index = Array.IndexOf(code, ch);
+
+                //Edge detection
+                if (index == 0)
+                {
+                    index = code.Length;
+                }
+                else if (index == code.Length)
+                {
+                    index = 0;
+                }
+
+                //Add 1 for the next number in alphabet
+                newString += code[index + 1];
+
+            }
+            return newString;
+        }
+
+        public static string Decrypt(string message)
+        {
+            char[] messageChar = message.ToCharArray();
+            string newString = "";
+            for (int i = 0; i < messageChar.Length; i++)
+            {
+                //Make it in uppercase when searching
+                char ch = Char.ToUpper(messageChar[i]);
+                //Find index in array where character is present
+                int index = Array.IndexOf(code, ch);
+
+                //Edge detection
+                if (index == 0)
+                {
+                    index = code.Length;
+                }
+                else if(index == code.Length)
+                {
+                    index = 0;
+                }
+
+                //Remove 1 for the normal alphabet
+                newString += code[index - 1];
+
+            }
+            return newString;
+        }
+    }
+}
---
+++ Encryption/Randomness/Randomness/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Randomness
+{
+    internal class Program
+    {
+        const int numberAmount = 100000;
+        static void Main(string[] args)
+        {
+            Stopwatch swCryp = new Stopwatch();
+            Stopwatch swRnd = new Stopwatch();
+            swCryp.Start();
+
+            CryptoRandom(numberAmount);
+            swCryp.Stop();
+
+            swRnd.Start();
+
+            RandomNumber(numberAmount);
+            swRnd.Stop();
+
+            Console.WriteLine($"Crypto random number took: {swCryp.Elapsed}");
+            Console.WriteLine($"Pseudo number took: {swRnd.Elapsed}");
+
+            Console.WriteLine($"Encryption of Hello: {Encrypter.Encrypt("Hello")}");
+            Console.WriteLine($"Decryption of IFMMP: {Encrypter.Decrypt("IFMMP")}");
+            Console.ReadLine();
+        }
+
+        private static void CryptoRandom(int numbers)
+        {
+            List<int> ints = new List<int>();
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] data = new byte[4];
+
+                for (int i = 0; i < numbers; i++)
+                {
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+
+                    rng.GetBytes(data);
+
+                    int value = BitConverter.ToInt32(data, 0);
+                    sw.Stop();
+
+                    ints.Add(value);
+                    //Console.WriteLine($"CRYPTO: {value} took: {sw.Elapsed}");
+                }
+            }
+
+            var grouped = ints.GroupBy(x => x).Select(x => new { Number = x.Key, Times = x.Count() }).ToList();
+
+            var groupedDoubleNumbers = grouped.Where(x => x.Times > 1);
+
+            Console.WriteLine("CRYPTO: Amount of times the same number appers");
+            foreach (var item in groupedDoubleNumbers)
+            {
+                Console.WriteLine($"CRYPTO: The amount of times a a number is the same: {item.Number} times: {item.Times}");
+            }
+        }
+
+        private static void RandomNumber(int numbers)
+        {
+            List<int> ints = new List<int>();
+
+            Random random = new Random();
+
+            for (int i = 0; i < numbers; i++)
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                int rnd = random.Next();
+
+                sw.Stop();
+
+                ints.Add(rnd);
+                //Console.WriteLine($"PSEUDO: {rnd} Took: {sw.Elapsed}");
+            }
+
+            var grouped = ints.GroupBy(x => x).Select(x => new { Number = x.Key, Times = x.Count() }).ToList();
+
+            var groupedDoubleNumbers = grouped.Where(x => x.Times > 1);
+
+            Console.WriteLine("PSEUDO: Amount of times the same number appers");
+            foreach (var item in groupedDoubleNumbers)
+            {
+                Console.WriteLine($"PSEUDO: The amount of times a a number is the same: {item.Number} times: {item.Times}");
+            }
+        }
+    }
+}
