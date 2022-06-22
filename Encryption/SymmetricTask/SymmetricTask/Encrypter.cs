b99c5f4e4809c2210137e1abe3cd60aeb0310240
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2022-06-22 13:05:29 +0200

    symmetric

---
+++ Encryption/SymmetricTask/SymmetricTask/Encrypter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetricTask
+{
+    public class Encrypter
+    {
+        public static byte[] Encrypt(SymmetricAlgorithm sa, string message)
+        {
+            //Convert message to byte array
+            byte[] messageByte = Encoding.ASCII.GetBytes(message);
+
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, sa.CreateEncryptor(), CryptoStreamMode.Write);
+
+            cs.Write(messageByte, 0, messageByte.Length);
+            cs.Close();
+
+            return ms.ToArray();
+        }
+
+        public static string Decrypt(SymmetricAlgorithm sa, string message)
+        {
+            byte[] decrypted = new byte[message.Length];
+            //Convert message to byte array
+            byte[] messageByte = Convert.FromBase64String(message);
+
+            using (MemoryStream ms = new MemoryStream(messageByte))
+            {
+                using (CryptoStream cs = new CryptoStream(ms, sa.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader((Stream)cs))
+                    {
+                        return sr.ReadToEnd();
+                    }
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Encryption/SymmetricTask/SymmetricTask/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SymmetricTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //Get user symmetric method
+                SymmetricAlgorithm sa = EncryptionMethodMenu();
+                Stopwatch sw = new Stopwatch();
+
+                if (SelectTypeMenu() == "1")
+                {
+                    string msg = AskForMessage();
+
+                    //Start timer and begin encryption
+                    sw.Start();
+                    byte[] encryptedMsg = Encrypter.Encrypt(sa, msg);
+                    sw.Stop();
+
+                    //Print result
+                    PrintResult(Convert.ToBase64String(encryptedMsg), sa.Key, sa.IV, sw.Elapsed);
+
+                }
+                else
+                {
+                    string msg = AskForMessage();
+
+                    Console.WriteLine("Write the key");
+                    string key = Console.ReadLine();
+
+                    Console.WriteLine("Write the IV");
+                    string iV = Console.ReadLine();
+
+                    //Set user defined key and IV
+                    sa.Key = Convert.FromBase64String(key);
+                    sa.IV = Convert.FromBase64String(iV);
+
+                    //Start timer and begin decryption
+                    sw.Start();
+                    string decryptedMsg = Encrypter.Decrypt(sa, msg);
+                    sw.Stop();
+                    
+                    PrintResult(decryptedMsg, sa.Key, sa.IV,sw.Elapsed);
+                }
+
+            }
+
+        }
+
+        private static void PrintResult(string encryptedMsg, byte[] key, byte[] iv, TimeSpan time)
+        {
+            string hexMsg = Convert.ToHexString(Encoding.ASCII.GetBytes(encryptedMsg));
+            string keyMsg = Convert.ToBase64String(key);
+            string iV = Convert.ToBase64String(iv);
+
+            Console.WriteLine(
+                $"\nASCII: {encryptedMsg}" +
+                $"\nHEX: {hexMsg}" +
+                $"\nKEY: {keyMsg}" +
+                $"\nIV: {iV}" +
+                $"\nTIME: {time}");
+        }
+
+        static string AskForMessage()
+        {
+            Console.WriteLine("Write a message");
+            return Console.ReadLine();
+        }
+
+        static string SelectTypeMenu()
+        {
+            Console.WriteLine("" +
+                "\n1: Encrypt" +
+                "\n2: Decrypt");
+
+            return Console.ReadLine();
+        }
+
+        static SymmetricAlgorithm EncryptionMethodMenu()
+        {
+            Console.WriteLine("" +
+                "\nSelect Method" +
+                "\n1: DES" +
+                "\n2: 3DES" +
+                "\n3: AES" +
+                "\n4: RC2" +
+                "\n5: Rijndael");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    return DES.Create();
+                case "2":
+                    return TripleDES.Create();
+                case "3":
+                    return Aes.Create();
+                case "4":
+                    return RC2.Create();
+                case "5":
+                    return Rijndael.Create();
+                default:
+                    return Aes.Create();
+            }
+        }
+
+
+        
+    }
+}
