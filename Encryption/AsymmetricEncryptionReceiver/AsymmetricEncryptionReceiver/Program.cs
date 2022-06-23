117cb06fef8a542250eb10d3517a0a22255699b0
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2022-06-23 12:40:14 +0200

    ASYMMETRIC ENCRYPTION

---
+++ Encryption/AsymmetricEncryptionReceiver/AsymmetricEncryptionReceiver/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AsymmetricEncryptionReceiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (RSACryptoServiceProvider myRSA = new RSACryptoServiceProvider(4096))
+            {
+
+                //I use false, because i dont need the private params to show
+                var rsaParams = myRSA.ExportParameters(false);
+
+                //Print params
+                PrintParameters(rsaParams);
+
+                while (true)
+                {
+                    //Got to remove the - for the fromhexstring to work
+                    Console.WriteLine("Write message to decrypt");
+                    string msgToDecrypt = Console.ReadLine().Replace("-", "");
+
+                    //Convert msg to bytes
+                    byte[] msgBytes = Convert.FromHexString(msgToDecrypt);
+
+                    //Decrypt the message
+                    byte[] decryptedMsg = myRSA.Decrypt(msgBytes, true);
+
+                    Console.WriteLine(Encoding.UTF8.GetString(decryptedMsg));
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+
+        private static void PrintParameters(RSAParameters rsaParams)
+        {
+            string exponent = BitConverter.ToString(rsaParams.Exponent);
+            string modulus = BitConverter.ToString(rsaParams.Modulus);
+
+
+            Console.WriteLine($"\nExponent: {exponent}" +
+                $"\nModulus: {modulus}");
+        }
+    }
+}
---
+++ Encryption/AsymmetricEncryptionSender/AsymmetricEncryptionSender/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AsymmetricEncryptionSender
+{
+    internal class Program
+    {
+        /// <summary>
+        /// This program can encrypt
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            RSAParameters rsaParams = AskRSAParams();
+            //Make sure rsa gets disposed automatically
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(4096))
+            {
+                //Import the parameters
+                rsa.ImportParameters(rsaParams);
+
+                while (true)
+                {
+                    Console.WriteLine("Write message to encrypt");
+
+                    string msg = Console.ReadLine();
+
+                    byte[] msgBytes = Encoding.ASCII.GetBytes(msg);
+
+                    //Encrypt the message with the public key we got.
+                    byte[] encryptedMsgByte = rsa.Encrypt(msgBytes, true);
+
+                    string encryptedMsg = BitConverter.ToString(encryptedMsgByte);
+                    Console.WriteLine($"\nEncryptedMsg: {encryptedMsg}");
+
+                }
+            }
+
+
+        }
+
+        private static RSAParameters AskRSAParams()
+        {
+            Console.WriteLine("Insert Exponent:");
+            //Replace the - with nothing, since it is not needed when converting to hex string
+            string exponent = Console.ReadLine().Replace("-", "");
+
+            Console.WriteLine("Insert Modulus");
+            string modulus = Console.ReadLine().Replace("-", "");
+
+            //Set the exponent and modulus from the generated public key, from the other program
+            RSAParameters rsaParams = new RSAParameters
+            {
+                Exponent = Convert.FromHexString(exponent),
+                Modulus = Convert.FromHexString(modulus)
+            };
+            return rsaParams;
+        }
+    }
+}
